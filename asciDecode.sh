ccb4ba8d88ec908d7b896d45b7635c0dc5695f71
yunustzr <yunus.tuzer@hotmail.com>
2022-10-19 00:34:05 +0300

    Update asciDecode.sh

--- asciDecode.sh
+++ asciDecode.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
+rm -rf message*
 
-chr() {
-  printf \\$(printf '%03o' $1)
+function chr {
+  if [ "$1" == "1000" ]
+  then
+    FILE=`dirname $0`/output
+    echo "$(cat output)"
+    zenity --text-info  --title="Message" --filename=$FILE
+    printf "\n--endMesage----\n"
+    kill -9 `pidof tcpdump`
+    exit 0
+  else
+    printf "\x$(printf %x $1)" >> output
+  fi
 }
+
+tcpdump -i any icmp -w message.pcap &  >/dev/null 2>&1
 echo "Gonderilen Mesaj"
-for i in $(cat message.txt |grep request|grep length|awk '{print $14}')
+while true
 do
-   chr $i
+  rm -rf output
+  tcpdump -r message.pcap >message.txt & >/dev/null 2>&1
+  echo "-----------"
+  echo "$(cat message.txt)"
+  sleep 1
+  for i in $(cat message.txt |grep length|grep request|awk '{print $14 }')
+  do
+   chr "$i"
+  done
 done
-echo ""
